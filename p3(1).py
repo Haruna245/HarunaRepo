b88fa22ed738e0b4240351812bb50acf31c4487b
Haruna245 <69992226+Haruna245@users.noreply.github.com>
2021-01-26 11:26:17 +0000

    Add files via upload

---
+++ p3(1).py
@@ -0,0 +1,9 @@
+na = "fibonacci sequence"
+# na refers to the name of the string #
+def sq(n):
+    # n refers to the number of sequence #
+    for i in range(n):
+        print(na[i],end='')
+
+
+p_view = sq(10)
