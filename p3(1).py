8a30673a655de67b8efc7365c054033454d8ebcf
Haruna245 <69992226+Haruna245@users.noreply.github.com>
2021-01-26 18:15:55 +0000

    Update p3(1).py

--- p3(1).py
+++ p3(1).py
@@ -1,7 +1,7 @@
-na = "fibonacci sequence"
-# na refers to the name of the string #
+name_str = "fibonacci sequence"
+# name_str refers to the name of the string #
 def sq(n):
-    # n refers to the number of sequence #
+    # n refers to the number of item you want to print #
     for i in range(n):
         print(na[i],end='')
 
